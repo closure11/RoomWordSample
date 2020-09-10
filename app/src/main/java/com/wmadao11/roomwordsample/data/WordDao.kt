2eba5ab5851fa75da548d7bd3bb2221869802607
zhouyisheng <wmadao11@gmail.com>
2020-09-10 20:40:30 +0800

    Add data layer

---
+++ app/src/main/java/com/wmadao11/roomwordsample/data/WordDao.kt
@@ -0,0 +1,17 @@
+package com.wmadao11.roomwordsample.data
+
+import androidx.room.*
+
+@Dao
+interface WordDao {
+
+    @Query("SELECT * FROM WORD_TABLE ORDER BY word ASC")
+    fun getAlphabetizedWords(): List<Word>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM WORD_TABLE")
+    suspend fun deleteAll()
+
+}
